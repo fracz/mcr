2128a56dd3be8358e349d80ca6f2a517aec43388
Wojciech Fracz <wojciech@fracz.pl>
2013-12-31 12:17:40 +0100

    Basic support for voice comments

---
+++ src/main/java/pl/fracz/mcr/fragment/RecordCommentPrompt.java
@@ -0,0 +1,79 @@
+package pl.fracz.mcr.fragment;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.actionbarsherlock.app.SherlockDialogFragment;
+import com.googlecode.androidannotations.annotations.EFragment;
+
+import java.io.File;
+import java.io.IOException;
+
+import pl.fracz.mcr.preferences.ApplicationSettings;
+
+@EFragment
+public class RecordCommentPrompt extends SherlockDialogFragment {
+
+    private MediaRecorder recorder;
+
+    private File recordedFile;
+
+    public static RecordCommentPrompt newInstance() {
+        return new RecordCommentPrompt();
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return new AlertDialog.Builder(getActivity())
+                // .setIcon(R.drawable.alert_dialog_icon)
+                .setTitle("Record a comment")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        stopRecording();
+                        recordedFile.renameTo(new File(ApplicationSettings.getApplicationDirectory(), "record.3gp"));
+                        dismiss();
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        stopRecording();
+                        recordedFile.delete();
+                        dismiss();
+                    }
+                }).create();
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startRecording();
+    }
+
+    private void startRecording() {
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        try {
+            recordedFile = File.createTempFile("dupa", "kupa");
+            recorder.setOutputFile(recordedFile.getAbsolutePath());
+            recorder.prepare();
+        } catch (IOException e) {
+            Log.e("A", "prepare() failed", e);
+        }
+        recorder.start();
+    }
+
+    private void stopRecording() {
+        recorder.stop();
+        recorder.release();
+        recorder = null;
+    }
+}
